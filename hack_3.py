d0e1136c4035a10804325c2a499bcbf69d7a5fb3
Adrián Páez <adriansauce98@gmail.com>
2024-04-11 15:35:15 -0400

    Resto de ejercicios, faltan 2

--- hack_3.py
+++ hack_3.py
@@ -14,8 +14,36 @@ text: "qu" output => "Qv"
 text: "qux" output => "QvX" 
 """
 
-
+vowels = {
+    'a' : '@',
+    'e' : '3',
+    'i' : '¡',
+    'o' : '0',
+    'u' : 'v'
+}
+num = '123456789'
+r = vowels.items()
+print(r)
 def fn_hack_3(s):
-    result = s
-    #...
+    for x,y in vowels.items():
+        s = s.replace(x,y)
+    if s.startswith('3'):
+        result = s[0] + s[1].upper()
+    elif  len(s) > 4:
+        result = s[0].upper() + s[1:7] + s[7].upper()
+    elif len(s) ==3:
+        result = s[0].upper() + s[1:2] + s[2].upper()
+    elif len(s) == 2:
+        result = s[0].upper() + s[1]
+    
+        
+    print(result)
+    
+    
     return result
+
+fn_hack_3('fooziman')
+fn_hack_3('barziman')
+fn_hack_3('3q')
+fn_hack_3('qu')
+fn_hack_3('qvx')
--- hack_8.py
+++ hack_8.py
@@ -8,14 +8,38 @@ text: ["a","b"] output => ["2","1"]
 """
 
 
+
+
+
 def fn_hack_8(s):
     result = []
-    for x in s:
-        s.append()
-            
-            
+    num = ['1','2','3','4','5']
+    if len(s) >= 5:
+        for x, j in zip(s, num):
+            result.append(x + '-' + j)
+
+    elif len(s) == 4:
+        count = 0
+        for x in range(0,len(s)):
+            count += 1
+            result.append(f'{count}')
+
+    elif len(s) == 3:
+        for x, j in zip(s, num):
+            result.append(x + '-' +j)
+
+    elif len(s) == 2:
+        count = 0
+        for x in range(0,len(s)):
+            count += 1
+            result.append(f'{count}')
+
+    result.reverse()
     print(result)
     return result
 
 
 fn_hack_8(["a","b","c","d","e"])
+fn_hack_8(["a","b","c","d"])
+fn_hack_8(["a","b","c"])
+fn_hack_8(["a","b"])
